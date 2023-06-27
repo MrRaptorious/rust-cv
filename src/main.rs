08aa8852720f80814cf718b56fa795d51be98247
MrRaptorious <lorenz.schmitten@gmail.com>
2023-06-27 22:33:52 +0200

    Add to_gray filter

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 use png::{Decoder, OutputInfo};
+use rayon::prelude::*;
+use std::error::Error;
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
-use std::error::Error;
 
-fn main() -> Result<(),Box<dyn Error>>{
+fn main() -> Result<(), Box<dyn Error>> {
     const IMG_PATH: &str = "res/lenna.png";
     const OUTPUT_PATH: &str = "out/lenna.png";
 
-    let (buf, info) = load_image(IMG_PATH)?; 
-    let bytes = &buf[..info.buffer_size()];
+    let (mut buf, info) = load_image(IMG_PATH)?;
 
-    save_image(OUTPUT_PATH, bytes, info)?;
+    to_gray(&mut buf);
 
-    return Ok(());
+    save_image(OUTPUT_PATH, &buf, info)?;
+
+    Ok(())
 }
 
 /// Load the png from the specified path
@@ -22,15 +24,15 @@ fn load_image(path: &str) -> Result<(Vec<u8>, OutputInfo), Box<dyn Error>> {
     let mut reader = decoder.read_info()?;
     let mut buf = vec![0; reader.output_buffer_size()];
     let info = reader.next_frame(&mut buf)?;
-    
-    return Ok((buf,info))
+
+    Ok((buf, info))
 }
 
 /// save the image to the specified path
-fn save_image(path: &str, data: &[u8], info: OutputInfo) -> Result<(),Box<dyn Error>> {
+fn save_image(path: &str, data: &[u8], info: OutputInfo) -> Result<(), Box<dyn Error>> {
     let path = Path::new(path);
     let file = File::create(path)?;
-    let ref mut w = BufWriter::new(file);
+    let w = BufWriter::new(file);
 
     let mut encoder = png::Encoder::new(w, info.width, info.height);
     encoder.set_color(info.color_type);
@@ -38,7 +40,19 @@ fn save_image(path: &str, data: &[u8], info: OutputInfo) -> Result<(),Box<dyn Er
 
     let mut writer = encoder.write_header()?;
 
-    writer.write_image_data(&data)?;
+    writer.write_image_data(data)?;
 
-    return Ok(());
+    Ok(())
+}
+
+fn to_gray(img: &mut [u8]) {
+    img.par_chunks_mut(3).for_each(|pxl| match pxl {
+        [r, g, b] => {
+            let gray = (((*r as u16) + (*g as u16) + (*b as u16)) / 3) as u8;
+            *r = gray;
+            *g = gray;
+            *b = gray;
+        }
+        _ => unreachable!(),
+    });
 }
